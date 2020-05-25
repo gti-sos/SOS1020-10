14b1db5ed553c123ff5f0268a36144769acd34c0
Antonio Jose Jimenez Segovia <antoniojosejimenezsegovia@gmail.com>
2020-05-25 21:38:05 +0200

    quitamos app integraciones

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ const globalDivorcesAPI = require(path.join(__dirname,"./src/back/divorcesAPI"))
 
 var app = express();//
 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
-
 app.use(bodyParser.json());   //
 
 marriagesAPI(app);   
