2780fd0041ce7151c33fe79d0ab5e75eec7b8261
juanmacortes9 <juanma-99@hotmail.es>
2020-06-03 23:43:06 +0200

    video añadido al about

--- src/front/about.svelte
+++ src/front/about.svelte
@@ -12,5 +12,5 @@ import  {pop} from "svelte-spa-router";
     <p></p>
     <h4>Vídeo DEMO API: Suicidios Globales</h4>
     <li><a href="https://youtu.be/-W06s122NXA" rel="nofollow">https://youtu.be/-W06s122NXA</a> (Desarrollada por:  <a href="https://github.com/nonospe">Antonio Jose Jimenez Segovia</a>)</li>
-
+    <li><a href="https://www.youtube.com/watch?v=hNLX321kCy4&feature=youtu.be" rel="nofollow">https://www.youtube.com/watch?v=hNLX321kCy4&feature=youtu.be</a> (Desarrollada por:  <a href="https://github.com/juanmacortes9">Juan Manuel Cortés Alonso</a>)</li>
 </main>
