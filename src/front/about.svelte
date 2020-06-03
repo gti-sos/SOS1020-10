4130f260799f34152e5522b9d437a96ae35dcb2f
Antonio Jose Jimenez Segovia <antoniojosejimenezsegovia@gmail.com>
2020-06-03 23:57:45 +0200

    añado títulos

--- src/front/about.svelte
+++ src/front/about.svelte
@@ -12,6 +12,10 @@ import  {pop} from "svelte-spa-router";
     <p></p>
     <h4>Vídeo DEMO API: Suicidios Globales</h4>
     <li><a href="https://youtu.be/-W06s122NXA" rel="nofollow">https://youtu.be/-W06s122NXA</a> (Desarrollada por:  <a href="https://github.com/nonospe">Antonio Jose Jimenez Segovia</a>)</li>
+    <p></p>
+    <h4>Vídeo DEMO API: Divorcios Globales</h4>
     <li><a href="https://www.youtube.com/watch?v=hNLX321kCy4&feature=youtu.be" rel="nofollow">https://www.youtube.com/watch?v=hNLX321kCy4&feature=youtu.be</a> (Desarrollada por:  <a href="https://github.com/juanmacortes9">Juan Manuel Cortés Alonso</a>)</li>
+    <p></p>
+    <h4>Vídeo DEMO API: Matrimonios Globales</h4>
     <li><a href="https://www.youtube.com/watch?v=mqswRSRINrM" rel="nofollow">https://www.youtube.com/watch?v=mqswRSRINrM</a> (Desarrollada por:  <a href="https://github.com/rivadeneyraj">Jesús Vázquez Rivadeneyra</a>)</li>
 </main>
